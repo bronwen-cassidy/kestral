a4eaae4ce655f6602a1ebdcfae66b507c2f72015
bronwen-cassidy <bronwen.cassidy@gmail.com>
2015-07-10 17:22:28 +0100

    enhancements

--- kestral-views/src/main/webapp/js/services.js
+++ kestral-views/src/main/webapp/js/services.js
@@ -2,15 +2,15 @@
 var kestralServices = angular.module('kestralServices', [])
     .service('clientService', ['$http', function ($http) {
         var self = this;
-        $http.get('/kestral/clients/client/find/1').
-            success(function (data, status, headers, config) {
-                self.clients = data;
-            }).
-            error(function (data, status, headers, config) {
+        $http.get('/kestral/clients/client/find/1').then(
+            function (response) {
+                self.clients = response.data;
+            }, function(errResponse) {
                 console.log("error!!!");
                 self.errorMsg = "error";
             }
         );
+
         //var items = [ { id : 1 , user : {firstname : 'Item 0'}}, { id : 2 , user : {firstname : 'Item 1'}} ];
         this.list = function () {
             return self.clients;
